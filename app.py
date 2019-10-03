cd100f77fcd31d8604f18a0d503b1b80e743ca97
Mario McGee <mario.mcgee@students.makeschool.com>
2019-10-03 03:24:05 -0700

    13th

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlistr')
@@ -34,7 +35,8 @@ def playlists_submit():
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split(),
-        'ratings': request.form.get('ratings')
+        'ratings': request.form.get('ratings'),
+        'created_at': datetime.now()
     }
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
@@ -74,14 +76,21 @@ def playlists_delete(playlist_id):
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
-        comment = {
-        'title': request.form.get('title'),
-        'content': request.form.get('content'),
-        'playlist_id': ObjectId(request.form.get('playlist_id'))
+    comment = {
+    'title': request.form.get('title'),
+    'content': request.form.get('content'),
+    'playlist_id': ObjectId(request.form.get('playlist_id'))
     }
     print(comment)
     comment_id = comments.insert_one(comment).inserted_id
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
     
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
